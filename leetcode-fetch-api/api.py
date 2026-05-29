bfcc20496a9674d038eb0f2710fbcf1674e60980
Corbin <csplatti@gmail.com>
2026-05-28 19:10:07 -0700

    api script fetches user data

---
+++ leetcode-fetch-api/api.py
@@ -0,0 +1,30 @@
+import requests
+
+LC_USER_BASE_URL = "https://leetcode.com/graphql"
+TEST_USER = "csplatti"
+
+def getACRequest(username: str):
+    return {
+        "query": "\n    query recentAcSubmissions($username: String!, $limit: Int!) {\n  recentAcSubmissionList(username: $username, limit: $limit) {\n    id\n    title\n    titleSlug\n    timestamp\n  }\n}\n    ",
+        "variables": {
+            "username": username,
+            "limit": 15
+        },
+        "operationName": "recentAcSubmissions"
+    }
+
+
+def getUserData(username: str):
+    URL = LC_USER_BASE_URL # + "/" + TEST_USER
+    JSON = getACRequest(username)
+    HEADERS = {
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+    "Referer": "https://leetcode.com",
+    }
+
+    data = requests.post(URL, json=JSON, headers=HEADERS)
+    print(data)
+    print(data.json()['data']['recentAcSubmissionList'])
+
+getUserData("csplatti")
